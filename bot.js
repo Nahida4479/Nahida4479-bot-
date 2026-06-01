7d00681a84a651edf2b89b0cd5e1c7ee42ccca8e
Nahida4479 <szymon.cyb13@gmail.com>
2026-06-01 23:22:58 +0200

    Add economy system base

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 // token_bot token_db url_db
 import { createClient } from "@libsql/client";
 import "dotenv/config";
-import { Client, GatewayIntentBits, REST, Routes, SlashCommandBuilder} from "discord.js"
+import { Client, GatewayIntentBits, REST, Routes, SlashCommandBuilder, EmbedBuilder, AttachmentBuilder} from "discord.js"
 
 const db = createClient({
     url: process.env.url_db,
@@ -31,7 +31,7 @@ client.once("ready", async () => {
 
         new SlashCommandBuilder()
         .setName("skillissues")
-        .setDescription("Wieszak zjawia się z dupy i daje ci 5 Solid Dice"),
+        .setDescription("Wieszak zjawia się z az megafonem i daje ci 5 Solid Dice po tym jak zostałem ogłuszony"),
 
         new SlashCommandBuilder()
         .setName("pinkpawsheist")
@@ -47,7 +47,22 @@ client.once("ready", async () => {
 
         new SlashCommandBuilder()
         .setName("łowienie")
-        .setDescription("Zacznij łowić aby odebrać nagrody")
+        .setDescription("Zacznij łowić aby odebrać nagrody"),
+
+        new SlashCommandBuilder()
+        .setName("ntegra")
+        .setDescription("Ustaw kanał gry - Nte Gra")
+        .addChannelOption((opt) =>
+            opt.setName("kanal").setDescription("Wybierz kanał").setRequired(true)
+        ),
+
+        new SlashCommandBuilder()
+        .setName("administracja")
+        .setDescription("Ustaw role które mogą zarządzać botem")
+        .addRoleOption((opt) =>
+            opt.setName("rola").setDescription("Wybierz rolę").setRequired(true)
+    )
+
 
     ].map((cmd) => cmd.toJSON());
 
@@ -57,25 +72,59 @@ client.once("ready", async () => {
 
 client.login(process.env.token_bot);
 
+async function checkcooldown(userId, guildId, komenda, cooldownMs) {
+    const teraz = Date.now();
+    const wynik = await db.execute({
+        sql: "SELECT ostatnio FROM cooldowny WHERE user_id = ? AND guild_id = ? AND komenda = ?",
+        args: [userId, guildId, komenda],
+    });
+    if (wynik.rows.length > 0) {
+        const ostatnio = Number(wynik.rows[0].ostatnio);
+        const roznica = teraz - ostatnio;
+        if (roznica < cooldownMs) {
+            const pozostalo =  cooldownMs - roznica;
+            const minuty = Math.floor(pozostalo / 60000);
+            const sekundy = Math.floor((pozostalo % 60000) / 1000);
+            return `Poczekaj jeszcze **${minuty}m ${sekundy}s**!`;
+        }
+    }
+
+    await db.execute({
+        sql: "INSERT INTO cooldowny (user_id, guild_id, komenda, ostatnio) VALUES (?, ?, ?, ?) ON CONFLICT(user_id, guild_id, komenda) DO UPDATE SET ostatnio = ?",
+        args: [userId, guildId, komenda, teraz, teraz]
+    });
+
+    return null;
+}
+
+
+//Solid Dice 
+
+async function addSolidDice(userId, guildId, ilosc) {
+    await db.execute({
+        sql: "INSERT INTO ekonomia (user_id, guild_id, solid_dice) VALUES (?, ?, ?) ON CONFLICT(user_id, guild_id) DO UPDATE SET solid_dice = solid_dice + ?",
+        args: [userId, guildId, ilosc, ilosc],
+    });
+    
+}
+
+
 client.on("interactionCreate", async (interaction) => {
     if (!interaction.isChatInputCommand()) return;
 
-    if (interaction.commandName === "zapisz") {
-        const tekst = interaction.options.getString("tekst");
-    
-        await db.execute({
-            sql: "INSERT INTO notatki (user_id, tresc) VALUES (?, ?)",
-            args: [interaction.user.id, tekst],
-        });
-        await interaction.reply("Zapisano do bazy!");
+    const ustawienia = await db.execute({
+        sql: "SELECT kanal_id FROM serwery WHERE guild_id = ?",
+        args: [interaction.guild.id],
+    });
+
+    const komendyEkonomii = ["daily", "work", "skillissues", "pinkpawsheist", "kawiarnia", "delivery", "łowienie"];
+
+    if (komendyEkonomii.includes(interaction.commandName)) {
+        const kanal = ustawienia.rows[0]?.kanal_id;
+        if (kanal &&  interaction.channelId !== kanal) {
+            await interaction.reply({ content: `Te komendy możesz używać tylko na kanale <#${kanal}>!`, ephemeral: true});
+            return;
+        }
     }
 
-    if (interaction.commandName === "pobierz") {
-        const wynik = await db.execute({
-            sql: "SELECT tresc FROM notatki where user_id = ?",
-            args: [interaction.user.id],
-        });
-        const wiersze = wynik.rows.map((r) => r.tresc).join ("\n");
-        await interaction.reply(wiersze || "Brak zapisów.");
-    }
 });
